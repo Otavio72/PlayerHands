5bac594fc048bf823c40b60f71e8f32cd4da6e4d
otavio <otavioribeiro1932004@yahoo.com>
2025-09-10 15:01:14 -0300

    sistema de envio de videos adicionado com multer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 const path = require('path');
 const app = express();
 const port = 5000;
@@ -7,15 +8,33 @@ app.use(express.static(path.join(__dirname, 'styles')));
 app.use(express.static(path.join(__dirname, 'videos')));
 
 
+
+// MULTER CONFIGURATION
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'videos/');
+    },
+    filename: function(req,file,cb){
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
+
+// ROUTES
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/video',(req,res)=>{
+app.post('/upload', upload.single('video') , (req,res)=>{
+    if(!req.file) return res.status(400).send('No file uploaded.');
+    res.redirect(`/video/${req.file.filename}`);
+
+});
+
+app.get('/video/:filename',(req,res)=>{
     res.sendFile(path.join(__dirname, 'videoplayer.html'));
 });
 
 
-app.listen(port,()=>{
-    console.log("Servidor rodando");
-})
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
